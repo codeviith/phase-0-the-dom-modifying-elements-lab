f88b6f7afdf280599a8b5cf7e8c64466edd0289d
codeviith <codeviith@Cinya>
2023-07-25 14:39:15 -0400

    DOM Lab 2

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+
+let newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+newHeader.textContent = "Kevin Lin is the champion";
+
+
+
+
+
+
+// document.querySelector('h1')
+// newHeader.setAttribute("id", "victory");
+
+// let newId = document.createElement("id");
+
+// newHeader.tagName = 'id';
+// newHeader.id = "'victory'";
+
+// newHeader.getElementById("victory");
+
+// newHeader.getElementById("id", "'victory'");
+
+// // newHeader.append("'victory'", id);
+// newHeader.setAttribute(id, "'victory'");
+
+// newHeader.id("id='victory");
+
+// const newHeader1 = document.getElementById("h1");
+// document.body.h1.setAttribute("id", "'victory'");
+
+
+
+
+
+
+
